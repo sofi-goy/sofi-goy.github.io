f2c59c57b3adcfa43708450035da3e030d9ee765
sofi-goy <sofi.goy42@gmail.com>
2022-07-17 12:47:57 -0300

    Estudio: agregar  (algunos) controles

--- camera/camera.js
+++ camera/camera.js
@@ -1,21 +1,15 @@
+var chunk = 5;
+const chunkSlider = document.getElementById('chunk-slider');
+
 const video = document.getElementById('webcam');
 const canvas = document.getElementById('filtered');
 const ctx = canvas.getContext('2d');
 
-video.style.display = "none";
-canvas.style.transform = "scale(-1,1)";
+function newMemory() {
+    return new FrameMemory(video.videoWidth, video.videoHeight, Math.floor(video.videoHeight / chunk));
+}
 
-/* Setting up the constraint */
-var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
-var constraints = {
-    audio: false,
-    video: {
-        facingMode: facingMode
-    }
-};
-
-const chunk = 5;
-var memory = new FrameMemory(video.videoWidth, video.videoHeight, Math.floor(video.videoHeight / chunk));
+var memory = newMemory();
 
 function computeFrame() {
     // Get data from video element
@@ -29,8 +23,8 @@ function computeFrame() {
 
     for (var y = 0; y < newFrame.height; y++) {
         for (var x = 0; x < newFrame.width; x++) {
-            var pixel = memory.getPixelFromPast(x,y,Math.floor(y/chunk));
-            newFrame.setPixel(x,y,pixel);
+            var pixel = memory.getPixelFromPast(x, y, Math.floor(y / chunk));
+            newFrame.setPixel(x, y, pixel);
         }
     }
 
@@ -40,18 +34,52 @@ function computeFrame() {
 var streamOn = false;
 
 function timerCallback() {
-    if (!streamOn){
+    if (!streamOn) {
         streamOn = true;
-        memory = new FrameMemory(video.videoWidth, video.videoHeight, Math.floor(video.videoHeight / chunk));
+        memory = newMemory();
     }
     computeFrame();
     setTimeout(function () {
         self.timerCallback();
-    }, 25); // roughly 40 frames per second
+    }, 1); // roughly 40 frames per second
 }
 
-/* Stream it to video element */
-navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-    video.srcObject = stream;
-    video.addEventListener('play', timerCallback);
-})
+function fullscreen() {
+    if (canvas.webkitRequestFullScreen) {
+        canvas.webkitRequestFullScreen();
+    }
+    else {
+        canvas.mozRequestFullScreen();
+    }
+}
+
+function newChunk() {
+    chunk = chunkSlider.value;
+    memory = newMemory();
+    console.log("Changed chunk", chunk);
+}
+
+function setUp() {
+    /* Setting up the constraint */
+    var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+    var constraints = {
+        audio: false,
+        video: {
+            facingMode: facingMode
+        }
+    };
+
+    /* Stream it to video element */
+    navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+        video.srcObject = stream;
+        video.addEventListener('play', timerCallback);
+    })
+
+    /* Making controls functional */
+    var fullbtn = document.getElementById('fullscreen-btn');
+    fullbtn.addEventListener("click", fullscreen);
+    chunkSlider.addEventListener("input", newChunk);
+
+}
+
+setUp();
