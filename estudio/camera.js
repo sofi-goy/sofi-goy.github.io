a0e1dfe37c20a195881218c3aeccce0d41d1b550
sofi-goy <sofi.goy42@gmail.com>
2022-07-27 09:43:09 -0300

    camera/ -> estudio/

--- estudio/camera.js
+++ estudio/camera.js
@@ -12,20 +12,20 @@ function newMemory() {
 
 var memory = newMemory();
 
-function pastDepth(x,y,tipoFranja) {
+function pastDepth(x, y, tipoFranja) {
     if (tipoFranja == "Horizontal")
-    return Math.floor(y / chunk);
-    
+        return Math.floor(y / chunk);
+
     if (tipoFranja == "Vertical")
         return Math.floor(x / chunk);
-        
-        if (tipoFranja == "Radial"){
-            var newX = x - video.videoWidth / 2;
-            var newY = y - video.videoHeight / 2;
-            var dist = Math.sqrt(newX * newX + newY * newY);
-            return Math.floor(dist / chunk);
+
+    if (tipoFranja == "Radial") {
+        var newX = x - video.videoWidth / 2;
+        var newY = y - video.videoHeight / 2;
+        var dist = Math.sqrt(newX * newX + newY * newY);
+        return Math.floor(dist / chunk);
     }
-    
+
     return 0;
 }
 
@@ -33,17 +33,19 @@ function computeFrame() {
     // Get data from video element
     ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
     var data = ctx.getImageData(0, 0, video.videoWidth, video.videoHeight);
-    var frame = new Frame(video.videoWidth, video.videoHeight);
-    frame.setData(data.data);
-    
+    var frame = new Frame(video.videoWidth, video.videoHeight, data.data);
     memory.pushPresent(frame);
-    var newFrame = new Frame(video.videoWidth, video.videoHeight);
-    var tipoFranja = franjas.value
 
-    for (var y = 0; y < newFrame.height; y++) {
-        for (var x = 0; x < newFrame.width; x++) {
-            var newX = newFrame.width - x;
-            var pixel = memory.getPixelFromPast(newX, y, pastDepth(x,y,tipoFranja));
+    var newFrame = new Frame(video.videoWidth, video.videoHeight);
+    var tipoFranja = franjas.value;
+
+    var newX;
+    var pixel;
+
+    for (var x = 0; x < newFrame.width; x++) {
+        newX = newFrame.width - x;
+        for (var y = 0; y < newFrame.height; y++) {
+            pixel = memory.getPixelFromPast(newX, y, pastDepth(x, y, tipoFranja));
             newFrame.setPixel(x, y, pixel);
         }
     }
@@ -61,7 +63,7 @@ function timerCallback() {
     computeFrame();
     setTimeout(function () {
         self.timerCallback();
-    }, 1);
+    }, 20);
 }
 
 function fullscreen() {
--- estudio/frameMemory.js
+++ estudio/frameMemory.js
@@ -1,17 +1,13 @@
-class Pixel {
-    constructor (data) {
-        this.r = data[0];
-        this.g = data[1];
-        this.b = data[2];
-        this.a = data[3];
-    }
-}
-
 class Frame {
-    constructor (width, height) {
+    constructor (width, height, data) {
         this.width = width;
         this.height = height;
-        this.data = new Uint8ClampedArray(4 * width * height);
+
+        if (data === undefined)
+            this.data = new Uint8ClampedArray(4 * width * height);
+
+        else
+            this.data = data
     } 
 
     index (x,y) {
@@ -20,19 +16,15 @@ class Frame {
 
     getPixel(x,y) {
         var i = this.index(x,y);
-        return new Pixel(this.data.slice(i, i+4));
+        return [this.data[i], this.data[i+1], this.data[i+2], this.data[i+3]];
     }
 
     setPixel(x,y,pixel) {
         var i = this.index(x,y);
-        this.data[i] = pixel.r;
-        this.data[i+1] = pixel.g;
-        this.data[i+2] = pixel.b;
-        this.data[i+3] = pixel.a;
-    }
-
-    setData(data){
-        this.data = data;
+        this.data[i]   = pixel[0];
+        this.data[i+1] = pixel[1];
+        this.data[i+2] = pixel[2];
+        this.data[i+3] = pixel[3];
     }
 }
 
@@ -50,6 +42,7 @@ class FrameMemory {
 
     pushPresent(frame) {
         this.presente = (this.presente + 1) % this.depth;
+        this.frames[this.presente].data = undefined;
         this.frames[this.presente] = frame;
     }
 
